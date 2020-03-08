f1d9dbd974dc0f4d592a743ed7016b16add6c434
putudoni <putuwigadoni@yahoo.com>
2020-03-08 18:18:25 +0700

    Create class MessageCustomConverter

---
+++ src/com/pnwd/spring/activemq/MessageCustomConverter.java
@@ -0,0 +1,31 @@
+package com.pnwd.spring.activemq;
+
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.jms.support.converter.MessageConversionException;
+import org.springframework.jms.support.converter.MessageConverter;
+
+public class MessageCustomConverter implements MessageConverter {
+
+	@Override
+	public Object fromMessage(Message message) throws JMSException, MessageConversionException {
+		MapMessage mapMessage = (MapMessage) message;
+		Member member = new Member();
+		member.setName(mapMessage.getString("name"));
+		member.setEmail(mapMessage.getString("email"));
+		return member;
+	}
+
+	@Override
+	public Message toMessage(Object object, Session session) throws JMSException, MessageConversionException {
+		Member member = (Member) object;
+		MapMessage message = session.createMapMessage();
+		message.setString("name", member.getName());
+		message.setString("email", member.getEmail());
+		return message;
+	}
+
+}
