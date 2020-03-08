50dede2c2fbe4f14614d6bc3fd5259343db7243c
putudoni <putuwigadoni@yahoo.com>
2020-03-08 18:21:00 +0700

    Send messages

--- src/com/pnwd/spring/activemq/SimpleSpringJMSPublisher.java
+++ src/com/pnwd/spring/activemq/SimpleSpringJMSPublisher.java
@@ -3,14 +3,24 @@
  */
 package com.pnwd.spring.activemq;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 /**
  * @author Putu Ngurah Wigadoni
  *
  */
 public class SimpleSpringJMSPublisher {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-
+		GenericXmlApplicationContext appContext = new GenericXmlApplicationContext();
+		appContext.load("springConfigPublisher.xml");
+		appContext.refresh();
+		MessageSender message = appContext.getBean("messageSender",MessageSender.class);
+		Member member = new Member();
+		member.setName("james");
+		member.setEmail("james@mail.com");
+		message.sender(member);
 	}
 
 }
