45e769842a3aa76f0d591997826925fc0bec37ab
putudoni <putuwigadoni@yahoo.com>
2020-03-08 17:53:09 +0700

    Create class SimpleSpringJMSSubscriber

---
+++ src/com/pnwd/spring/activemq/SimpleSpringJMSSubscriber.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.pnwd.spring.activemq;
+
+/**
+ * @author Putu Ngurah Wigadoni
+ *
+ */
+public class SimpleSpringJMSSubscriber {
+
+	public static void main(String[] args) {
+
+	}
+
+}
