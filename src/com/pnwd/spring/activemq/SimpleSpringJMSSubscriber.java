3735276a8f53de31802ccfced9a5edefcd33d73e
putudoni <putuwigadoni@yahoo.com>
2020-03-08 18:36:19 +0700

    Test receive message

--- src/com/pnwd/spring/activemq/SimpleSpringJMSSubscriber.java
+++ src/com/pnwd/spring/activemq/SimpleSpringJMSSubscriber.java
@@ -3,14 +3,24 @@
  */
 package com.pnwd.spring.activemq;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 /**
  * @author Putu Ngurah Wigadoni
  *
  */
 public class SimpleSpringJMSSubscriber {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-
+		GenericXmlApplicationContext appContext = new GenericXmlApplicationContext();
+		appContext.load("springConfigSubscriber.xml");
+		appContext.refresh();
+		MessageReceiver message = appContext.getBean("messageReceiver",MessageReceiver.class);
+		Member member = message.getMessage();
+		System.out.println("Receive message");
+		System.out.println("Name  : " + member.getName());
+		System.out.println("Email  : " + member.getEmail());
 	}
 
 }
